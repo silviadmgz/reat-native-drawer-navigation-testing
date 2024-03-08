22ed609a51bfaaa69cb559c4c2bf5b257aa20e6c
Silvia Dominguez <silviadmgz@gmail.com>
2024-03-08 14:08:47 +0100

    add expo router drawer

---
+++ app/user.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function User() {
+	return (
+		<View style={{ flex: 1, alignContent: 'center', justifyContent: 'center' }}>
+			<Text>User Page</Text>
+		</View>
+	);
+}
